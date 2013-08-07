7b2e24733e6428fbc662ec93bc8e66656269c4ed
Cory <greenmachine@fullsail.edu>
2013-08-06 22:40:50 -0700

    Implementing a read method to read and return the data in storage

--- Week1Project/src/com/Cory/week1/MainActivity.java
+++ Week1Project/src/com/Cory/week1/MainActivity.java
@@ -9,6 +9,8 @@
  */
 package com.Cory.week1;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -47,6 +49,30 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    }
+    
+    private StringBuffer returnDataFromStorage(String fileName){
+    	FileInputStream fileInputStream;
+		try {
+			fileInputStream = openFileInput(fileName);
+		
+    	BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+    	byte[] contentBytes = new byte[1024];
+    	int bytesRead = 0;
+    	String content;
+    	StringBuffer contentBuffer = new StringBuffer();
+  
+		while((bytesRead = bufferedInputStream.read(contentBytes)) != -1){
+			content = new String(contentBytes,0,bytesRead);
+			contentBuffer.append(content);
+			}
+		return contentBuffer;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+    	
     }
 
 
