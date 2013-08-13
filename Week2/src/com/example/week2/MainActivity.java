f289205d52dfea40174fd461ee3b221789b38d59
Cory <greenmachine@fullsail.edu>
2013-08-13 16:16:02 -0700

    setup of my files again for week 2

--- Week2/src/com/example/week2/MainActivity.java
+++ Week2/src/com/example/week2/MainActivity.java
@@ -1,19 +1,60 @@
 package com.example.week2;
 
+import com.example.week2.WebInfo;
+import com.example.week2.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // created my xml layout
-        setContentView(R.layout.layout);
-    }
+    	String inputString;
+    	Boolean _connected = false;
+    	Context _context;
+    	
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // created my xml layout
+            setContentView(R.layout.layout);
+            
+            _context = this;
+            
+            // Detect network connection
+            _connected = WebInfo.getConnectionStatus(_context);
+             if(_connected)
+          	{
+          		Log.i("Network Connection", WebInfo.getConnectionType(_context));
+        	}
+            
+            
+            // setting up my gobutton
+            Button goButton = (Button) findViewById(R.id.goButton);
+            goButton.setOnClickListener(new OnClickListener(){
 
+    			@Override
+    			public void onClick(View v) {
+    			
+    				// gathering user input
+    		        EditText userEnteredField = (EditText) findViewById(R.id.userEnteredText);
+    		        inputString = userEnteredField.getText().toString();
+    				
+    				
+    				
+    			}
+            	
+            });
+        }
+        
+        
+        
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
